457d6beedbbd5ba1355f779784f241054a15f30c
Flávia Miranda <76967175+flaviagmiranda@users.noreply.github.com>
2021-10-27 19:31:09 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+const linksSocialMedia = {
+    github: 'flaviagmiranda',
+    youtube: 'UC46hB7TbdUavvMXgaSZVKew',
+    facebook: 'gracianofla',
+    instagram: 'flaviagraciano',
+    twitter: 'flaviagraciano'
+}
+
+function changeSocialMedia() {
+    for (let li of socialLinks.children) {
+        const social = li.getAttribute('class')
+        li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+    }
+}
+
+changeSocialMedia()
+
+function getGitHubProfileInfos() {
+    const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            userName.textContent = data.name
+            userBio.textContent = data.bio
+            userLink.href = data.html_url
+            userImage.src = data.avatar_url
+            userLogin.textContent = data.login
+        })
+}
+
+getGitHubProfileInfos()
